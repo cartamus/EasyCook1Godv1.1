503d83a8d9357ff80727678a36200f3fa4052eaa
SakJav <jesusalvarez.villegas@itsn.edu.mx>
2018-12-04 21:01:04 -0600

    Pruba Email 1

--- app/src/main/java/com/example/intertec/easycook/Activity/Login.java
+++ app/src/main/java/com/example/intertec/easycook/Activity/Login.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,9 +12,11 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.Toast;
 //si wey
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -24,6 +27,13 @@ import com.example.intertec.easycook.R;
 import com.example.intertec.easycook.Utileria.Util;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 private LoginButton loginButton;
@@ -34,6 +44,9 @@ private CallbackManager callbackManager;
     private Button login;
     private  Button registro;
     private Switch recuerdame;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +61,8 @@ private CallbackManager callbackManager;
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-gotoMain();
+
+                handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
             @Override
@@ -61,6 +75,16 @@ Toast.makeText(getApplicationContext(),R.string.cancel_Login,Toast.LENGTH_SHORT)
                 Toast.makeText(getApplicationContext(),R.string.error_Login,Toast.LENGTH_SHORT).show();
             }
         });
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    gotoMain();
+                }
+            }
+        };
         bindUI();
 
         prefs = getSharedPreferences("preferencias", Context.MODE_PRIVATE);
@@ -88,6 +112,25 @@ Toast.makeText(getApplicationContext(),R.string.cancel_Login,Toast.LENGTH_SHORT)
 
 
     }
+
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        progressBar.setVisibility(View.VISIBLE);
+        loginButton.setVisibility(View.GONE);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), R.string.firebase_error_login, Toast.LENGTH_LONG).show();
+                }
+                progressBar.setVisibility(View.GONE);
+                loginButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+
 
     private void bindUI() {
         edittextEmail = (EditText) findViewById(R.id.mail);
@@ -155,7 +198,17 @@ Toast.makeText(getApplicationContext(),R.string.cancel_Login,Toast.LENGTH_SHORT)
         super.onActivityResult(requestCode,resultCode,data);
         callbackManager.onActivityResult(requestCode,resultCode,data);
 }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+    }
 
 
 }
--- app/src/main/java/com/example/intertec/easycook/Activity/MainActivity.java
+++ app/src/main/java/com/example/intertec/easycook/Activity/MainActivity.java
@@ -4,16 +4,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.intertec.easycook.Activity.Login;
 import com.example.intertec.easycook.R;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 private SharedPreferences prefs;
+    private TextView nameTextView;
+    private TextView emailTextView;
+    private TextView uidTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,27 @@ private SharedPreferences prefs;
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         prefs = getSharedPreferences("preferencias", Context.MODE_PRIVATE);
+        nameTextView = (TextView) findViewById(R.id.nameTextView);
+        emailTextView = (TextView) findViewById(R.id.emailTextView);
+        uidTextView = (TextView) findViewById(R.id.uidTextView);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+            String uid = user.getUid();
+
+            nameTextView.setText(name);
+            emailTextView.setText(email);
+            uidTextView.setText(uid);
+        } else {
+            goLoginScreen();
+        }
+    }
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, Login.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     @Override
@@ -33,25 +63,29 @@ private SharedPreferences prefs;
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_logout:
-                logout();
+                logout1();
                 return true;
             case R.id.forget_logout:
                 removeSharePreferences();
-                logout();
+                logout1();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
-
-
-
         }
     }
-    private void logout(){
+    private void logout1(){
+
         Intent intent=new Intent(this, Login.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);//sirve para que cierre la aplicacion y no no regrese al login
         startActivity(intent);
+
     }
     private void removeSharePreferences(){
         prefs.edit().clear().apply();//mas simplificado que en el login
+    }
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        LoginManager.getInstance().logOut();
+        goLoginScreen();
     }
 }
