ef25d85c4b211733b6c55d63144a27424f646c7f
SakJav <jesusalvarez.villegas@itsn.edu.mx>
2018-12-04 18:47:34 -0600

    Pruba Email 1

--- app/src/main/java/com/example/intertec/easycook/Activity/Registro.java
+++ app/src/main/java/com/example/intertec/easycook/Activity/Registro.java
@@ -1,15 +1,88 @@
 package com.example.intertec.easycook.Activity;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.intertec.easycook.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registro extends AppCompatActivity {
+    //defining view objects
+    private EditText TextEmail;
+    private EditText TextPassword;
+    private Button btnRegistrar;
+    private ProgressDialog progressDialog;
 
+
+    //Declaramos un objeto firebaseAuth
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        //inicializamos el objeto firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //Referenciamos los views
+        TextEmail = (EditText) findViewById(R.id.mail2);
+        TextPassword = (EditText) findViewById(R.id.password2);
+
+        btnRegistrar = (Button) findViewById(R.id.button_login2);
+
+        progressDialog = new ProgressDialog(this);
+    }
+    private void registrarUsuario(){
+
+        //Obtenemos el email y la contraseña desde las cajas de texto
+        String email = TextEmail.getText().toString().trim();
+        String password  = TextPassword.getText().toString().trim();
+
+        //Verificamos que las cajas de texto no esten vacías
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Se debe ingresar un email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Falta ingresar la contraseña",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        progressDialog.setMessage("Realizando registro en linea...");
+        progressDialog.show();
+
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+
+                            Toast.makeText(Registro.this,"Se ha registrado el usuario con el email: "+ TextEmail.getText(),Toast.LENGTH_LONG).show();
+                        }else{
+
+                            Toast.makeText(Registro.this,"No se pudo registrar el usuario ",Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+
+}
+
+    public void onClick(View view) {
+        //Invocamos al método:
+        registrarUsuario();
     }
 }
