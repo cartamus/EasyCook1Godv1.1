0f23b1b1c2048f611f38cda8212b416a9d3d9f7c
cartamus <carlosau2@hotmail.com>
2018-12-06 10:40:58 -0600

    algo bien

---
+++ app/src/main/java/com/example/intertec/easycook/Base/NoteViewHolder.java
@@ -0,0 +1,40 @@
+package com.example.intertec.easycook.Base;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.intertec.easycook.R;
+
+/**
+ * Created by SUDA on 08-09-2017.
+ */
+
+public class NoteViewHolder extends RecyclerView.ViewHolder {
+
+    View mView;
+
+    TextView textTitle, textTime;
+    CardView noteCard;
+
+    public NoteViewHolder(View itemView) {
+        super(itemView);
+
+        mView = itemView;
+
+        textTitle = mView.findViewById(R.id.note_title);
+        textTime = mView.findViewById(R.id.note_time);
+        noteCard = mView.findViewById(R.id.note_card);
+
+    }
+
+    public void setNoteTitle(String title) {
+        textTitle.setText(title);
+    }
+
+    public void setNoteTime(String time) {
+        textTime.setText(time);
+    }
+
+}
